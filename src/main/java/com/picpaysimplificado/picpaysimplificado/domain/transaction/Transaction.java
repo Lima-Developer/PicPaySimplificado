05fa16c27907daf81f2c3237257d5f2515b82b21
Lima-Developer <jvflima14@gmail.com>
2024-05-13 11:17:50 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,38 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne //Um usuário pode ter muitas transações mas uma transação só pode pertencer a apenas um sender e um receiver
+    @JoinColumn(name = "receiver_id")//Informa que a chave estrangeira da entidade User será chamada de 'receiver_id'
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+    public Transaction() {
+
+    }
+}
